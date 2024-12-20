9eacef3f2ad92cdea0094b91f76dc4a7716b95f1
Alex Kontis <alex@alexkontis.co.uk>
2024-12-20 12:06:43 +0000

    creates ability to comment on articles

--- src/components/CommentsView.jsx
+++ src/components/CommentsView.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getComments } from "../../api";
 import { SkeletonCard } from './SkeletonCard'
+import { NewCommentForm } from './NewCommentForm'
 import '../styles/CommentsView.css'
 
 export const CommentsView = ({ article_id }) => {
@@ -18,27 +19,47 @@ export const CommentsView = ({ article_id }) => {
         setIsLoading(false)
     }
 
+    const addComment = (newComment) => {
+        if (comments.length === 0) {
+            return setComments([newComment])
+        }
+        return setComments([newComment, ...comments])
+    }
+
     useEffect(() => {
         getAllComments()
     }, [article_id])
 
-    const renderCommentContent = () => {
+    const renderComments = () => {
         if (isLoading) {
             return <SkeletonCard />
         }
 
         if (comments.length === 0) {
-            return <h3>No comments on this post yet</h3>
+            return (
+                <>
+                    <h3>Be the first to comment</h3>
+                    <NewCommentForm article_id={article_id} addComment={addComment} />
+                </>
+            )
         }
 
-        return comments.map((comment, index) => (
-            <div className='comment-area' key={comment.id || index}>
-                <p className="comment-body">{comment.body}</p>
-                <p className="comment-author">
-                    <b><i>{comment.author}</i></b>
-                </p>
-            </div>
-        ))
+        return (
+            <>
+                <h3>Add your voice to the conversation</h3>
+                <NewCommentForm article_id={article_id} addComment={addComment} />
+                {
+                    comments.map((comment, index) => (
+                        <div className='comment-area' key={comment.id || index}>
+                            <p className="comment-body">{comment.body}</p>
+                            <p className="comment-author">
+                                <b><i>{comment.author}</i></b>
+                            </p>
+                        </div>
+                    ))
+                }
+            </>
+        )
     }
 
     return (
@@ -46,7 +67,7 @@ export const CommentsView = ({ article_id }) => {
             <hr style={{ maxWidth: '250px' }} />
             <h2>Comments</h2>
             <div className="comments-list">
-                {renderCommentContent()}
+                {renderComments()}
             </div>
         </section>
     )
